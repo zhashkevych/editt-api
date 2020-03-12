f0c9f9d87bc187b85c6d137a07ca70b70a8a3ab8
Max Zhashkevych <maxzhashkevych@Maxs-MacBook-Air.local>
2020-03-12 10:35:45 +0200

    publication sanitization and additional fields setting implemented in usecase

--- publication/usecase/usecase.go
+++ publication/usecase/usecase.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"edittapi/models"
 	"edittapi/publication"
+	"github.com/microcosm-cc/bluemonday"
+	"strings"
+	"time"
 )
 
+const averageReadingSpeed = 200 // Words per minute
+
 type PublicationUseCase struct {
-	repo publication.Repository
+	repo   publication.Repository
+	policy *bluemonday.Policy
 }
 
 func NewPublicationUseCase(repo publication.Repository) *PublicationUseCase {
@@ -17,6 +23,10 @@ func NewPublicationUseCase(repo publication.Repository) *PublicationUseCase {
 }
 
 func (p PublicationUseCase) Publish(ctx context.Context, publication models.Publication) error {
+	publication.PublishedAt = time.Now()
+	publication.Body = p.policy.Sanitize(publication.Body)
+	publication.ReadingTime = estimateReadingTime(publication.Body)
+
 	return p.repo.Create(ctx, publication)
 }
 
@@ -26,4 +36,10 @@ func (p PublicationUseCase) GetPopularPublications(ctx context.Context, limit in
 
 func (p PublicationUseCase) GetLatestPublications(ctx context.Context, limit int64) ([]*models.Publication, error) {
 	return p.repo.GetLatest(ctx, limit)
+}
+
+func estimateReadingTime(text string) int32 {
+	wordsCount := len(strings.Split(text, ""))
+
+	return int32(wordsCount / averageReadingSpeed)
 }
