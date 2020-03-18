99db393a9f31a3342423405b04819643ab4b8a0e
medstar <medstar@MacBook-Pro-MedStar.local>
2020-03-18 14:45:16 +0200

    added prod deployment image and configuration

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-build:
+build-local:
 	go mod download && CGO_ENABLED=0 GOOS=linux go build -o ./.bin/app ./cmd/api/main.go
 
-run: build
+run-local: build-local
 	docker-compose up --remove-orphans --build server
+
+build-deploy:
+	docker image build -t editt:0.1 -f ./deploy/Dockerfile .
+
+run-deploy:
+	export HOST=prod && docker run -e HOST --publish 8000:8000 --name editt-api editt:0.1
--- config/init.go
+++ config/init.go
@@ -2,11 +2,22 @@ package config
 
 import (
 	"github.com/spf13/viper"
+	"os"
 )
 
 func Init() error {
 	viper.AddConfigPath("./config")
-	viper.SetConfigName("config")
 
-	return viper.ReadInConfig()
+	viper.SetConfigName("config")
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	env := os.Getenv("HOST")
+	if env == "" {
+		env = "local"
+	}
+
+	viper.SetConfigName(env)
+	return viper.MergeInConfig()
 }
