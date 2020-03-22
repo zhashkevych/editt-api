2c0aca0ee364b4ce9384e5bff025f37cada3807e
zhashkevych <zhashkevychmaksim@gmail.com>
2020-03-22 16:58:39 +0200

    fixed sh script

--- run-container.sh
+++ run-container.sh
@@ -2,5 +2,6 @@ if [ "$(docker ps -q -f name=editt-api)" ]; then
     if [ ! "$(docker ps -aq -f status=exited -f name=editt-api)" ]; then
         docker stop editt-api
     fi
-    docker run -e HOST --rm -d --publish 8000:8000 --network editt --name editt-api editt:0.1
 fi
+
+docker run -e HOST --rm -d --publish 8000:8000 --network editt --name editt-api editt:0.1
